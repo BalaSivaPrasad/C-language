6d12fb8aae285437bec86f7655eadcd24287f241
BalaSivaPrasad <balasivaprasad4@gmail.com>
2025-03-09 19:29:11 +0530

    Create temp.c
    temparature >35 or not

---
+++ temp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define Pi =3.14;
+void main() {
+    float temp=0.0;
+    printf("Enter the temprature:");
+    scanf("%f",& temp);
+    if(temp>35){
+        printf("yes");
+    }
+    else{
+        printf("No");
+    }
+}
