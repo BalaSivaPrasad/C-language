9e3ed436b2a3be458efa89c956773e72a2484ee6
BalaSivaPrasad <balasivaprasad4@gmail.com>
2025-03-09 18:56:02 +0530

    Create Addition.c
    Addition of integers

---
+++ Addition.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main() {
+    int a=0,b=0,c=0,d=0;
+    printf("Enter three numbers:");
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    d=a+b+c;
+    printf("%d",d);
+}
