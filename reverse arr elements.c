27b3c587d8719bb5acbbf54485a104ec75b0220a
BalaSivaPrasad <balasivaprasad4@gmail.com>
2025-03-18 15:50:14 +0530

    Create reverse arr elements.c
    print array elements in reverse order

---
+++ reverse arr elements.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main() {
+    int ar[5],i;
+    printf("Enter five elements:");
+    for(i=0;i<=4;i++){
+        scanf("%d",&ar[i]);
+    }
+    printf("Given elements are:\n");
+    for(i=4;i>=0;i--){
+        printf("%d\t",ar[i]);
+    }
+}
