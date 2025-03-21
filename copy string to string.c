de9179b1dd1c40d8257e8226b244cd098faeb1cd
BalaSivaPrasad <balasivaprasad4@gmail.com>
2025-03-21 19:19:14 +0530

    Create copy string to string.c
    copy contents of 1 string to another string

---
+++ copy string to string.c
@@ -0,0 +1,9 @@
+ #include <stdio.h>
+ #include <string.h>
+ int main(){
+     char source[]="Hello,World!";
+     char destination[20];
+     strcpy(destination,source);
+     printf("copied string:%s\n",destination);
+     return 0;
+ }
