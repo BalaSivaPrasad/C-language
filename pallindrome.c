0280069e3eaa64fba52329691228d787b14ff098
BalaSivaPrasad <balasivaprasad4@gmail.com>
2025-03-24 14:46:58 +0530

    Update pallindrome.c
    a number is pallindrome or not

--- pallindrome.c
+++ pallindrome.c
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+int main() {
+    int num, dup;
+    int rev = 0, rem; 
+    printf("Enter a Number: ");
+    scanf("%d", &num);
+    dup = num;
+    while (dup > 0) {
+        rem = dup % 10;
+        rev = rev * 10 + rem;
+        dup = dup / 10;
+    }
+    if (num == rev)
+        printf("Palindrome\n");
+    else
+        printf("Not Palindrome\n");
+    return 0;
+}
