7ccc4729eaa1055a5025ef442d8d03988cebc256
BalaSivaPrasad <balasivaprasad4@gmail.com>
2025-03-26 22:19:58 +0530

    Create access ele pointer.c
    Access array elements using pointers

---
+++ access ele pointer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void geeks()
+{
+	int val[3] = { 5, 10, 15 };
+	int* ptr;
+	ptr = val;
+	printf("Elements of the array are: ");
+	printf("%d, %d, %d", ptr[0], ptr[1], ptr[2]);
+	return;
+}
+int main()
+{
+	geeks();
+	return 0;
+}
