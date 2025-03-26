4ffc5392e5a530df88dc0c3a8977724cbcf38676
BalaSivaPrasad <balasivaprasad4@gmail.com>
2025-03-26 22:24:27 +0530

    Create use unioun.c
    demonstrate to use unioun

---
+++ use unioun.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+union un {
+	int member1;
+	char member2;
+	float member3;
+};
+int main()
+{
+	union un var1;
+	var1.member1 = 15;
+	printf("The value stored in member1 = %d",
+		var1.member1);
+	return 0;
+}
